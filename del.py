1da78c66a5c8726e6e8c924396721a90001f6103
Cody Zacharias <cody@live.se>
2018-04-02 13:33:47 -0400

    Create del.py

---
+++ del.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+# Usage: .del (number) or .del 
+import asyncio
+import discord
+import re
+
+c = discord.Client()
+token = "INSERT_TOKEN_HERE"
+
+@c.event
+async def on_ready():
+    welcome = "Logged in as {0.name} - {0.id}".format(c.user)
+    print(welcome)
+
+@c.event
+async def on_message(message):
+    if message.content.startswith('.del') and message.author == c.user:
+        if re.search(r'\d+$', message.content) is not None:
+            t = int(message.content[len('.del'):].strip())
+        else:
+            t = 9999
+        async for m in c.logs_from(message.channel,limit=t):
+            try:
+                if m.author == c.user:
+                    await c.delete_message(m)
+            except: pass
+
+c.run(token, bot=False)
